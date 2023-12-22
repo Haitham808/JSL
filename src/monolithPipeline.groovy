b3d17aac3b7f48b830930c1370593026fb4ad4e1
“mahmoud <haitham.mahmoud@gumtreegroup.com.au>
2023-12-22 14:37:56 +1100

    aa

--- src/monolithPipeline.groovy
+++ src/monolithPipeline.groovy
@@ -6,26 +6,24 @@ def call() {
     // Global.set(this, params)
 
     pipeline {
-        kubernetes {
-            label "haitham-test"
-            yaml """
-            apiVersion: v1
-            kind: Pod
-            metadata:
-              name: nginx-echo-hello-world
-            spec:
-              containers:
-                - name: nginx
-                  image: nginx:latest
-                  command: ["/bin/sh"]
-                  args:
-                    - "-c"
-                    - |
-                      echo "Hello World";
-                      while true; do sleep 3600; done
-                    """
-        }
-
+        
+        agent {
+            kubernetes {
+                    label "haitham-test"
+                    yaml """
+                    apiVersion: v1
+                    kind: Pod
+                    metadata:
+                    name: nginx-echo-hello-world
+                    spec:
+                    containers:
+                      - name: nginx
+                        image: nginx:latest
+                        command: ["/bin/sh"]
+                        args: ["sleep","3600"]
+                           """
+              }
+          }
         stages {
             stage('Hello') {
                 steps {
